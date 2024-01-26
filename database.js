c7b9804ded3fa04c72580a201f12e9855a6b2d3f
RahulDawpersad <135977072+RahulDawpersad@users.noreply.github.com>
2024-01-26 21:07:56 +0200

    this is madness

--- database.js
+++ database.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql2');
 
-const db = mysql.createConnection({
+const db = mysql.createPool({
   host: 'b3j3ueoaxfknmx0du16n-mysql.services.clever-cloud.com',
   user: 'ukvxj5gys9pcsfkj',
   password: 'csj17lHJGWlS5FEktS3u',
   database: 'b3j3ueoaxfknmx0du16n'
-});
+});;
 
 db.connect((err) => {
   if (err) {
