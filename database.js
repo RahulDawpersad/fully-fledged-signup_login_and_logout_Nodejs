c5a7a2b077289b592459072987d51c7debb1a327
Rahul Dawpersad <135977072+RahulDawpersad@users.noreply.github.com>
2024-01-26 17:36:13 +0200

    Update database.js

--- database.js
+++ database.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 const db = mysql.createConnection({
   host: 'b3j3ueoaxfknmx0du16n-mysql.services.clever-cloud.com',
   user: 'ukvxj5gys9pcsfkj',
-  password: 'ukvxj5gys9pcsfkj',
+  password: 'csj17lHJGWlS5FEktS3u',
   database: 'b3j3ueoaxfknmx0du16n'
 });
 
